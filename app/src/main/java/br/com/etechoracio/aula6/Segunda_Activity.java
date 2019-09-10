533338f202805c9eedaff36d5d6bf2ff26979018
lucasmartins2506 <lucasnalua25@gmail.com>
2019-09-09 21:54:01 -0300

    Criacao segunda activity

---
+++ app/src/main/java/br/com/etechoracio/aula6/Segunda_Activity.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.aula6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Segunda_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda_);
+    }
+}
